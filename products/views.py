f5f522417978b9d244ef1ea96f9f318fddb37bd7
Cr15t1an0 <cristian.enicarp@mail.ru>
2021-12-06 18:19:28 +0200

    lesson1 first commit

--- products/views.py
+++ products/views.py
@@ -7,3 +7,21 @@ def index(request):
 
 def products(requests):
     return render(requests, 'products/products.html')
+
+def test_context(requests):
+    context = {
+        'title': 'store',
+        'header': 'Добро пожаловать!',
+        'username': 'Иван Иванов',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090.00},
+            {'name': 'Синяя куртка The North Face', 'price': 23725.00},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390.00},
+        ],
+        'promotion': True,
+        'products_of_promotion': [
+            {'name': 'Черный рюкзак Nike Heritage', 'price': 2340.00},
+        ]
+
+    }
+    return render(requests, 'products/test-context.html', context)
